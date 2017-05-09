f85465f22b158a72417d6c02e28f28ad54f38cb3
phil-krull <pkrull@codingdojo.com>
2017-05-09 15:44:06 -0700

    added demos folder

---
+++ demos/checkerboard.py
@@ -0,0 +1,13 @@
+# for i in range(20):
+#     str_builder = ''
+#     if i%2 is not 0:
+#         str_builder += ' '
+#     for j in range(7):
+#         str_builder += '* '
+#     print str_builder
+
+
+
+for i in range(0,4):
+   print('* * * * ')
+   print(' * * * *')
